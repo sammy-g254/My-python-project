e499613476a663bcb53d65fc177dc22c79d0154d
sammy-g254 <gsam65303@gmail.com>
2025-01-31 10:36:36 +0300

    Renewed

--- Basics.py
+++ Basics.py
@@ -1,1 +1,17 @@
 print("Hello World")
+
+#variables
+fname="Sam"
+lname="G"
+age=18
+course="fullstack"
+
+#displaying the variable value
+print(fname)
+print(lname)
+print(age)
+print(course)
+
+#displaying a string and a variable
+print("Hello my name is",fname)
+print("I am",age,"years old")
