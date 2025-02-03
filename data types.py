250cfa3895067cb7972e728da186e18e131caad1
sammy-g254 <gsam65303@gmail.com>
2025-02-03 10:13:14 +0300

    new commit

---
+++ data types.py
@@ -0,0 +1,10 @@
+#datatypes
+first_name="Sam" #string
+age=20 #integer
+tax=10.5 #float
+pie=3.14
+#boolean
+is_student=True;
+greeting="hello ,world"
+fruits=["apples","oranges","mangoes","pineapple"] #list
+my_range=range(10) #range
---
+++ strings.py
@@ -0,0 +1,26 @@
+#string is a data type used to represent text
+greeting="hello world"
+course='python'
+print(greeting)
+print(course)
+#string edits
+#change case
+print(greeting.upper())
+print(greeting.lower())
+print(greeting.title())
+#length
+print(len(course))
+print(len(greeting))
+print(len("mark"))
+#string concatenation "+""
+fname="Sam"
+lname="G"
+print(fname+" "+lname)
+#escape characters "\"
+print("hello good morning, \nhow are you")
+print("name\tcourse")
+print('She actually "responded"')
+print('but truly, is it actually \"possible\"')
+print("Now that's it")
+print("she said \"hello\"")
+print("she said \"it's a nice day\"")
