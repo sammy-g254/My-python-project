6371b923301323232793a49a383f3f92832e3f13
sammy-g254 <gsam65303@gmail.com>
2025-02-03 10:33:35 +0300

    new commit 2

--- strings.py
+++ strings.py
@@ -24,3 +24,23 @@ print('but truly, is it actually \"possible\"')
 print("Now that's it")
 print("she said \"hello\"")
 print("she said \"it's a nice day\"")
+print("""
+      This 
+      is 
+      an 
+      example 
+      of 
+      a 
+      multiline 
+      string""")
+#string indexing starts from 0
+text=('python')
+print(text[1])
+print(text[3])
+message="hello"
+print(message[-3],message[-3])
+#string slicing
+mytext="Fullstack"
+print(mytext[0:4]) #full
+print(mytext[4:9]) #stack
+print(mytext[0:5]) #fulls
